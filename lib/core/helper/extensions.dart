a8744556361e4b1d0b6a3c45e40acfd90ef003a5
HazimNabil <HazemNabil95@outlook.com>
2025-08-07 20:55:10 +0300

    feat: add ThemeExtension

---
+++ lib/core/helper/extensions.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+extension ThemeExtension on BuildContext {
+  Color get primaryColor => Theme.of(this).colorScheme.primary;
+  Color get textColor => Theme.of(this).colorScheme.secondary;
+  Color get actionColor => Theme.of(this).colorScheme.tertiary;
+  Color get surfaceColor => Theme.of(this).colorScheme.surface;
+  Color get subTextColor => Theme.of(this).hintColor;
+}
