61af397401323e2f5f6d59372442ba4aaf6fefa5
HazimNabil <HazemNabil95@outlook.com>
2025-08-07 19:18:50 +0300

    feat: generate assets paths

---
+++ lib/core/helper/assets.dart
@@ -0,0 +1,25 @@
+class Assets {
+  Assets._();
+
+  static const iconsCart = "assets/icons/cart.svg";
+
+  static const iconsFavorite = "assets/icons/favorite.svg";
+
+  static const iconsFilter = "assets/icons/filter.svg";
+
+  static const iconsHome = "assets/icons/home.svg";
+
+  static const iconsProfile = "assets/icons/profile.svg";
+
+  static const iconsSearch = "assets/icons/search.svg";
+
+  static const imagesLamp = "assets/images/lamp.jpeg";
+
+  static const imagesOnBoardingOne = "assets/images/on_boarding_one.svg";
+
+  static const imagesOnBoardingThree = "assets/images/on_boarding_three.svg";
+
+  static const imagesOnBoardingTwo = "assets/images/on_boarding_two.svg";
+
+  static const imagesSplash = "assets/images/splash.png";
+}
