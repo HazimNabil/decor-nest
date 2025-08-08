c217af2b808d74dacc36e2fac88b891fc39537f6
HazimNabil <HazemNabil95@outlook.com>
2025-08-08 22:18:50 +0300

    feat: add DevicePreview to app

--- lib/decor_nest.dart
+++ lib/decor_nest.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:decor_nest/core/themes/app_themes.dart';
 import 'package:decor_nest/core/router/app_router.dart';
+import 'package:device_preview/device_preview.dart';
 
 class DecorNest extends StatelessWidget {
   const DecorNest({super.key});
@@ -9,8 +10,11 @@ class DecorNest extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp.router(
       debugShowCheckedModeBanner: false,
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       theme: AppThemes.light,
       darkTheme: AppThemes.dark,
+      themeMode: ThemeMode.light,
       routerConfig: AppRouter.router,
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
 import 'package:flutter/material.dart' show runApp;
+import 'package:flutter/foundation.dart' show kReleaseMode;
 import 'package:decor_nest/decor_nest.dart';
+import 'package:device_preview/device_preview.dart';
 
 void main() {
-  runApp(const DecorNest());
+  runApp(
+    DevicePreview(
+      enabled: !kReleaseMode,
+      builder: (context) => const DecorNest(),
+    ),
+  );
 }
