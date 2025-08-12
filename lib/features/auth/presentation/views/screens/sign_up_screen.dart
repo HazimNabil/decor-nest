c6bdae0a72cae96575a42b73a51fa6dad975a5fc
HazimNabil <HazemNabil95@outlook.com>
2025-08-12 23:12:45 +0300

    feat: add sign up buttons to SignUpScreen

--- lib/features/auth/presentation/views/screens/sign_up_screen.dart
+++ lib/features/auth/presentation/views/screens/sign_up_screen.dart
@@ -1,4 +1,6 @@
+import 'package:decor_nest/core/helper/extensions.dart';
 import 'package:decor_nest/core/themes/app_styles.dart';
+import 'package:decor_nest/core/widgets/custom_button.dart';
 import 'package:decor_nest/features/auth/presentation/views/widgets/sign_up_form.dart';
 import 'package:flutter/material.dart';
 
@@ -25,6 +27,19 @@ class SignUpScreen extends StatelessWidget {
                 ),
                 const SizedBox(height: 32),
                 const SignUpForm(),
+                const SizedBox(height: 24),
+                CustomButton(
+                  text: 'Sign Up',
+                  color: context.primaryColor,
+                  onPressed: () {},
+                ),
+                const SizedBox(height: 16),
+                CustomButton(
+                  text: 'Sign Up With Google',
+                  color: context.surfaceColor,
+                  textColor: context.textColor,
+                  onPressed: () {},
+                ),
               ],
             ),
           ),
