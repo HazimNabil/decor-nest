ea7ff89d1c8b8f662a7e12e3d97d389cede637db
HazimNabil <HazemNabil95@outlook.com>
2025-08-12 20:54:48 +0300

    feat: finish LoginScreen UI

--- lib/features/auth/presentation/views/screens/login_screen.dart
+++ lib/features/auth/presentation/views/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:decor_nest/core/helper/extensions.dart';
 import 'package:decor_nest/core/themes/app_styles.dart';
 import 'package:decor_nest/core/widgets/custom_button.dart';
 import 'package:decor_nest/features/auth/presentation/views/widgets/forgot_password_option.dart';
@@ -13,12 +14,11 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Center(
           child: SingleChildScrollView(
             child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text('Welcome Back', style: AppStyles.semiBold32(context)),
@@ -27,12 +27,23 @@ class LoginScreen extends StatelessWidget {
                   'Welcome Back! Please Enter Your Details.',
                   style: AppStyles.regular16(context),
                 ),
-                const SizedBox(height: 32),
+                const SizedBox(height: 48),
                 const LoginForm(),
                 const SizedBox(height: 16),
                 const ForgotPasswordOption(),
-                const SizedBox(height: 24),
-                CustomButton(text: 'Login', onPressed: () {}),
+                const SizedBox(height: 48),
+                CustomButton(
+                  text: 'Sign In',
+                  color: context.primaryColor,
+                  onPressed: () {},
+                ),
+                const SizedBox(height: 16),
+                CustomButton(
+                  text: 'Sign In With Google',
+                  color: context.surfaceColor,
+                  textColor: context.textColor,
+                  onPressed: () {},
+                ),
                 const SizedBox(height: 24),
                 const SignUpOption(),
               ],
