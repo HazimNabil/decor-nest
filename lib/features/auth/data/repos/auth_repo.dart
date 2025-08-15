bdefe46f0f27e90baf93b719dee4292220fc0173
HazimNabil <HazemNabil95@outlook.com>
2025-08-16 01:26:03 +0300

    feat: create AuthRepo interface

---
+++ lib/features/auth/data/repos/auth_repo.dart
@@ -0,0 +1,12 @@
+import 'package:decor_nest/features/auth/data/models/login_input_data.dart';
+import 'package:decor_nest/features/auth/data/models/sign_up_input_data.dart';
+import 'package:fpdart/fpdart.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:decor_nest/core/helper/typedefs.dart';
+
+abstract interface class AuthRepo {
+  FutureEither<User> logIn(LoginInputData loginInputData);
+  FutureEither<Unit> signUp(SignUpInputData signUpInputData);
+  FutureEither<Unit> logOut();
+  FutureEither<Unit> resetPassword(String email);
+}
