c97101e83327a7bb0770ddca190cb4df9a833d0b
HazimNabil <HazemNabil95@outlook.com>
2025-08-15 03:44:09 +0300

    feat: initialize Supabase

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,18 @@ import 'package:flutter/foundation.dart' show kReleaseMode;
 import 'package:decor_nest/decor_nest.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:decor_nest/core/helper/cache_helper.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:decor_nest/core/helper/app_secrets.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await CacheHelper.init();
+  await Future.wait([
+    CacheHelper.init(),
+    Supabase.initialize(
+      url: AppSecrets.supabaseUrl,
+      anonKey: AppSecrets.supabaseAnonKey,
+    ),
+  ]);
   runApp(
     DevicePreview(
       enabled: !kReleaseMode,
