748eac4ecb79c1c4ff39c9209285a8f5ccd09f5f
HazimNabil <HazemNabil95@outlook.com>
2025-08-16 01:57:08 +0300

    refactor: extract SignUpScreenBody as separated widget

---
+++ lib/features/auth/presentation/views/widgets/sign_up_screen_body.dart
@@ -0,0 +1,49 @@
+import 'package:decor_nest/core/helper/extensions.dart';
+import 'package:decor_nest/core/themes/app_styles.dart';
+import 'package:decor_nest/core/widgets/custom_button.dart';
+import 'package:decor_nest/features/auth/presentation/views/widgets/login_option.dart';
+import 'package:decor_nest/features/auth/presentation/views/widgets/sign_up_form.dart';
+import 'package:flutter/material.dart';
+
+class SignUpScreenBody extends StatelessWidget {
+  const SignUpScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 24),
+      child: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Create Account', style: AppStyles.semiBold32(context)),
+              const SizedBox(height: 8),
+              Text(
+                'Let\'s create account together',
+                style: AppStyles.regular16(context),
+              ),
+              const SizedBox(height: 32),
+              const SignUpForm(),
+              const SizedBox(height: 24),
+              CustomButton(
+                text: 'Sign Up',
+                color: context.primaryColor,
+                onPressed: () {},
+              ),
+              const SizedBox(height: 16),
+              CustomButton(
+                text: 'Sign Up With Google',
+                color: context.surfaceColor,
+                textColor: context.textColor,
+                onPressed: () {},
+              ),
+              const SizedBox(height: 24),
+              const LoginOption(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
