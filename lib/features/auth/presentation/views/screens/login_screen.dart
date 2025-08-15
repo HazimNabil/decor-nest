344a3cec4d94fba57e6dac6c762ddb81bed62701
HazimNabil <HazemNabil95@outlook.com>
2025-08-16 01:54:57 +0300

    refactor: extract LoginScreenBody as separated widget

--- lib/features/auth/presentation/views/screens/login_screen.dart
+++ lib/features/auth/presentation/views/screens/login_screen.dart
@@ -1,9 +1,4 @@
-import 'package:decor_nest/core/helper/extensions.dart';
-import 'package:decor_nest/core/themes/app_styles.dart';
-import 'package:decor_nest/core/widgets/custom_button.dart';
-import 'package:decor_nest/features/auth/presentation/views/widgets/forgot_password_option.dart';
-import 'package:decor_nest/features/auth/presentation/views/widgets/login_form.dart';
-import 'package:decor_nest/features/auth/presentation/views/widgets/sign_up_option.dart';
+import 'package:decor_nest/features/auth/presentation/views/widgets/login_screen_body.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -13,44 +8,6 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24),
-        child: Center(
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Welcome Back', style: AppStyles.semiBold32(context)),
-                const SizedBox(height: 8),
-                Text(
-                  'Welcome Back! Please Enter Your Details.',
-                  style: AppStyles.regular16(context),
-                ),
-                const SizedBox(height: 48),
-                const LoginForm(),
-                const SizedBox(height: 16),
-                const ForgotPasswordOption(),
-                const SizedBox(height: 48),
-                CustomButton(
-                  text: 'Sign In',
-                  color: context.primaryColor,
-                  onPressed: () {},
-                ),
-                const SizedBox(height: 16),
-                CustomButton(
-                  text: 'Sign In With Google',
-                  color: context.surfaceColor,
-                  textColor: context.textColor,
-                  onPressed: () {},
-                ),
-                const SizedBox(height: 24),
-                const SignUpOption(),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+    return const Scaffold(body: LoginScreenBody());
   }
 }
---
+++ lib/features/auth/presentation/views/widgets/login_screen_body.dart
@@ -0,0 +1,52 @@
+import 'package:decor_nest/core/helper/extensions.dart';
+import 'package:decor_nest/core/themes/app_styles.dart';
+import 'package:decor_nest/core/widgets/custom_button.dart';
+import 'package:decor_nest/features/auth/presentation/views/widgets/login_form.dart';
+import 'package:decor_nest/features/auth/presentation/views/widgets/forgot_password_option.dart';
+import 'package:decor_nest/features/auth/presentation/views/widgets/sign_up_option.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreenBody extends StatelessWidget {
+  const LoginScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('Welcome Back', style: AppStyles.semiBold32(context)),
+              const SizedBox(height: 8),
+              Text(
+                'Welcome Back! Please Enter Your Details.',
+                style: AppStyles.regular16(context),
+              ),
+              const SizedBox(height: 48),
+              const LoginForm(),
+              const SizedBox(height: 16),
+              const ForgotPasswordOption(),
+              const SizedBox(height: 48),
+              CustomButton(
+                text: 'Sign In',
+                color: context.primaryColor,
+                onPressed: () {},
+              ),
+              const SizedBox(height: 16),
+              CustomButton(
+                text: 'Sign In With Google',
+                color: context.surfaceColor,
+                textColor: context.textColor,
+                onPressed: () {},
+              ),
+              const SizedBox(height: 24),
+              const SignUpOption(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
