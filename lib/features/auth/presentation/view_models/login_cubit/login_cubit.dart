05b6cc14eca67d126da642b213366359eaa336cb
HazimNabil <HazemNabil95@outlook.com>
2025-08-16 18:42:50 +0300

    feat: add logIn method to LoginCubit

--- lib/features/auth/presentation/view_models/login_cubit/login_cubit.dart
+++ lib/features/auth/presentation/view_models/login_cubit/login_cubit.dart
@@ -1,10 +1,23 @@
+import 'package:decor_nest/features/auth/data/repos/auth_repo.dart';
 import 'package:flutter/foundation.dart' show immutable;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:supabase_flutter/supabase_flutter.dart' show User;
+import 'package:decor_nest/features/auth/data/models/login_input_data.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
-  LoginCubit() : super(const LoginInitial());
+  final AuthRepo _authRepo;
+  LoginCubit(this._authRepo) : super(const LoginInitial());
+
+  Future<void> logIn(LoginInputData loginInputData) async {
+    emit(const LoginLoading());
+
+    final result = await _authRepo.logIn(loginInputData);
+    result.fold(
+      (failure) => emit(LoginFailure(failure.message)),
+      (user) => emit(LoginSuccess(user)),
+    );
+  }
 }
