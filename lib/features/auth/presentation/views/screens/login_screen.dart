c777613b702d9d14ae9e16e175740e8dd7fa94f2
HazimNabil <HazemNabil95@outlook.com>
2025-08-16 18:51:16 +0300

    feat: provide LoginCubit to LoginScreenBody

--- lib/features/auth/presentation/views/screens/login_screen.dart
+++ lib/features/auth/presentation/views/screens/login_screen.dart
@@ -1,5 +1,9 @@
+import 'package:decor_nest/core/di/service_locator.dart';
+import 'package:decor_nest/features/auth/data/repos/auth_repo_impl.dart';
 import 'package:decor_nest/features/auth/presentation/views/widgets/login_screen_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:decor_nest/features/auth/presentation/view_models/login_cubit/login_cubit.dart';
 
 class LoginScreen extends StatelessWidget {
   static const path = '/login';
@@ -8,6 +12,11 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: LoginScreenBody());
+    return Scaffold(
+      body: BlocProvider(
+        create: (context) => LoginCubit(locator<AuthRepoImpl>()),
+        child: const LoginScreenBody(),
+      ),
+    );
   }
 }
