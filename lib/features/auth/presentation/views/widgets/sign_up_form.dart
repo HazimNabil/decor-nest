b7073eac7074814f6bf0f65e8e76c00ce7c74d99
HazimNabil <HazemNabil95@outlook.com>
2025-08-17 01:48:00 +0300

    feat: add validation to SignUpForm

--- lib/features/auth/presentation/views/widgets/sign_up_form.dart
+++ lib/features/auth/presentation/views/widgets/sign_up_form.dart
@@ -1,31 +1,91 @@
 import 'package:decor_nest/core/themes/app_styles.dart';
 import 'package:decor_nest/core/widgets/custom_text_field.dart';
+import 'package:decor_nest/features/auth/data/models/sign_up_input_data.dart';
 import 'package:flutter/material.dart';
 
 class SignUpForm extends StatelessWidget {
-  const SignUpForm({super.key});
+  final GlobalKey<FormState> formKey;
+  final AutovalidateMode autovalidateMode;
+  final SignUpInputData signUpInputData;
+
+  const SignUpForm({
+    super.key,
+    required this.formKey,
+    required this.autovalidateMode,
+    required this.signUpInputData,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text('Name', style: AppStyles.medium14(context)),
-        const SizedBox(height: 8),
-        const CustomTextField(hint: 'Enter your name'),
-        const SizedBox(height: 16),
-        Text('Email', style: AppStyles.medium14(context)),
-        const SizedBox(height: 8),
-        const CustomTextField(hint: 'Enter your email'),
-        const SizedBox(height: 16),
-        Text('Password', style: AppStyles.medium14(context)),
-        const SizedBox(height: 8),
-        const CustomTextField(hint: 'Enter your password', isPassword: true),
-        const SizedBox(height: 16),
-        Text('Confirm Password', style: AppStyles.medium14(context)),
-        const SizedBox(height: 8),
-        const CustomTextField(hint: 'Confirm your password', isPassword: true),
-      ],
+    return Form(
+      key: formKey,
+      autovalidateMode: autovalidateMode,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('Name', style: AppStyles.medium14(context)),
+          const SizedBox(height: 8),
+          CustomTextField(hint: 'Enter your name', validator: _nameValidator),
+          const SizedBox(height: 16),
+          Text('Email', style: AppStyles.medium14(context)),
+          const SizedBox(height: 8),
+          CustomTextField(hint: 'Enter your email', validator: _emailValidator),
+          const SizedBox(height: 16),
+          Text('Password', style: AppStyles.medium14(context)),
+          const SizedBox(height: 8),
+          CustomTextField(
+            hint: 'Enter your password',
+            isPassword: true,
+            validator: _passwordValidator,
+          ),
+          const SizedBox(height: 16),
+          Text('Confirm Password', style: AppStyles.medium14(context)),
+          const SizedBox(height: 8),
+          CustomTextField(
+            hint: 'Confirm your password',
+            isPassword: true,
+            validator: _confirmPasswordValidator,
+          ),
+        ],
+      ),
     );
+  }
+
+  String? _nameValidator(String? name) {
+    if (name?.isEmpty ?? true) {
+      return 'Please enter your name';
+    }
+    return null;
+  }
+
+  String? _emailValidator(String? email) {
+    final emailRegex = RegExp(
+      r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$',
+    );
+
+    if (email?.isEmpty ?? true) {
+      return 'Please enter your email';
+    } else if (!emailRegex.hasMatch(email!)) {
+      return 'Please enter a valid email';
+    }
+    return null;
+  }
+
+  String? _passwordValidator(String? password) {
+    if (password?.isEmpty ?? true) {
+      return 'Please enter your password';
+    } else if (password!.length < 8) {
+      return 'Password must be at least 8 characters long';
+    }
+    return null;
+  }
+
+  String? _confirmPasswordValidator(String? confirmPassword) {
+    if (confirmPassword?.isEmpty ?? true) {
+      return 'Please enter your confirm password';
+    } else if (confirmPassword != signUpInputData.password) {
+      return 'Passwords do not match';
+    }
+    return null;
   }
 }
--- lib/features/auth/presentation/views/widgets/sign_up_screen_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_up_screen_body.dart
@@ -1,12 +1,36 @@
 import 'package:decor_nest/core/helper/extensions.dart';
 import 'package:decor_nest/core/themes/app_styles.dart';
 import 'package:decor_nest/core/widgets/custom_button.dart';
+import 'package:decor_nest/features/auth/data/models/sign_up_input_data.dart';
 import 'package:decor_nest/features/auth/presentation/views/widgets/login_option.dart';
 import 'package:decor_nest/features/auth/presentation/views/widgets/sign_up_form.dart';
 import 'package:flutter/material.dart';
 
-class SignUpScreenBody extends StatelessWidget {
+class SignUpScreenBody extends StatefulWidget {
   const SignUpScreenBody({super.key});
+
+  @override
+  State<SignUpScreenBody> createState() => _SignUpScreenBodyState();
+}
+
+class _SignUpScreenBodyState extends State<SignUpScreenBody> {
+  late final GlobalKey<FormState> _formKey;
+  late final ValueNotifier<AutovalidateMode> _autovalidateMode;
+  late final SignUpInputData _signUpInputData;
+
+  @override
+  void initState() {
+    super.initState();
+    _formKey = GlobalKey<FormState>();
+    _autovalidateMode = ValueNotifier(AutovalidateMode.disabled);
+    _signUpInputData = SignUpInputData();
+  }
+
+  @override
+  void dispose() {
+    _autovalidateMode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,12 +48,25 @@ class SignUpScreenBody extends StatelessWidget {
                 style: AppStyles.regular16(context),
               ),
               const SizedBox(height: 32),
-              const SignUpForm(),
+              ValueListenableBuilder(
+                valueListenable: _autovalidateMode,
+                builder: (_, value, _) => SignUpForm(
+                  formKey: _formKey,
+                  autovalidateMode: value,
+                  signUpInputData: _signUpInputData,
+                ),
+              ),
               const SizedBox(height: 24),
               CustomButton(
                 text: 'Sign Up',
                 color: context.primaryColor,
-                onPressed: () {},
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    // Sign up
+                  } else {
+                    _autovalidateMode.value = AutovalidateMode.always;
+                  }
+                },
               ),
               const SizedBox(height: 16),
               CustomButton(
