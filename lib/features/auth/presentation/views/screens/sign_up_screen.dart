8b2e5e9a5435df9ad55342b84cdad148ce189a27
HazimNabil <HazemNabil95@outlook.com>
2025-08-17 04:40:37 +0300

    feat: provide SignUpCubit to SignUpScreenBody

--- lib/features/auth/presentation/views/screens/sign_up_screen.dart
+++ lib/features/auth/presentation/views/screens/sign_up_screen.dart
@@ -1,5 +1,9 @@
 import 'package:decor_nest/features/auth/presentation/views/widgets/sign_up_screen_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:decor_nest/features/auth/presentation/view_models/sign_up_cubit/sign_up_cubit.dart';
+import 'package:decor_nest/core/di/service_locator.dart';
+import 'package:decor_nest/features/auth/data/repos/auth_repo_impl.dart';
 
 class SignUpScreen extends StatelessWidget {
   static const path = '/sign_up';
@@ -8,6 +12,11 @@ class SignUpScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: SafeArea(child: SignUpScreenBody()));
+    return Scaffold(
+      body: BlocProvider(
+        create: (context) => SignUpCubit(locator<AuthRepoImpl>()),
+        child: const SafeArea(child: SignUpScreenBody()),
+      ),
+    );
   }
 }
