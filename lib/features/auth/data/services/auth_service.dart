c0d79155fee28fec109fbc78c191a37ef0e96c4c
HazimNabil <HazemNabil95@outlook.com>
2025-08-19 00:10:21 +0300

    feat: add logInWithGoogle method to AuthService

--- lib/features/auth/data/services/auth_service.dart
+++ lib/features/auth/data/services/auth_service.dart
@@ -1,9 +1,11 @@
 import 'package:decor_nest/features/auth/data/models/login_input_data.dart';
 import 'package:decor_nest/features/auth/data/models/sign_up_input_data.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class AuthService {
   final _supabaseAuth = Supabase.instance.client.auth;
+  final _googleSignIn = GoogleSignIn();
 
   User? get currentUser => _supabaseAuth.currentUser;
 
@@ -23,7 +25,33 @@ class AuthService {
     );
   }
 
-  Future<void> logOut() async => await _supabaseAuth.signOut();
+  Future<User?> logInWithGoogle() async {
+    final googleUser = await _googleSignIn.signIn();
+
+    final googleAuth = await googleUser?.authentication;
+    final accessToken = googleAuth?.accessToken;
+    final idToken = googleAuth?.idToken;
+
+    if (accessToken == null) {
+      throw 'No Access Token found.';
+    }
+    if (idToken == null) {
+      throw 'No ID Token found.';
+    }
+
+    final response = await _supabaseAuth.signInWithIdToken(
+      provider: OAuthProvider.google,
+      idToken: idToken,
+      accessToken: accessToken,
+    );
+
+    return response.user;
+  }
+
+  Future<void> logOut() async {
+    await _googleSignIn.disconnect();
+    await _supabaseAuth.signOut();
+  }
 
   Future<void> resetPassword(String email) async {
     await _supabaseAuth.resetPasswordForEmail(email);
