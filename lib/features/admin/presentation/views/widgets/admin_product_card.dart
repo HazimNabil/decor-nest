a85584f3ad51fdeed382d96ac9b424e33af4771f
HazimNabil <HazemNabil95@outlook.com>
2025-08-21 19:31:37 +0300

    feat: make AdminProductCard slidable

--- lib/features/admin/presentation/views/widgets/admin_product_card.dart
+++ lib/features/admin/presentation/views/widgets/admin_product_card.dart
@@ -1,19 +1,45 @@
 import 'package:decor_nest/core/helper/extensions.dart';
 import 'package:decor_nest/features/admin/presentation/views/widgets/product_card_info.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class AdminProductCard extends StatelessWidget {
   const AdminProductCard({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: context.surfaceColor,
-        borderRadius: BorderRadius.circular(14),
+    return Slidable(
+      endActionPane: ActionPane(
+        motion: const ScrollMotion(),
+        children: [
+          SlidableAction(
+            onPressed: (context) {},
+            backgroundColor: context.primaryColor,
+            foregroundColor: context.surfaceColor,
+            icon: Icons.edit,
+            label: 'Edit',
+          ),
+          SlidableAction(
+            onPressed: (context) {},
+            backgroundColor: Colors.red,
+            foregroundColor: context.surfaceColor,
+            borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(8),
+              bottomRight: Radius.circular(8),
+            ),
+            icon: Icons.delete,
+            label: 'Delete',
+          ),
+        ],
       ),
-      padding: const EdgeInsets.all(14),
-      child: const ProductCardInfo(),
+      child: Container(
+        decoration: BoxDecoration(
+          color: context.surfaceColor,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        padding: const EdgeInsets.all(14),
+        child: const ProductCardInfo(),
+      ),
     );
   }
 }
