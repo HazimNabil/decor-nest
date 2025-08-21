7c07ddf14f8d2a4810d5c927a2777bdabb0fd662
HazimNabil <HazemNabil95@outlook.com>
2025-08-21 20:40:26 +0300

    feat: add app bar to EditProductScreen

--- lib/features/admin/presentation/views/screens/edit_product_screen.dart
+++ lib/features/admin/presentation/views/screens/edit_product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:decor_nest/core/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class EditProductScreen extends StatelessWidget {
@@ -7,6 +8,9 @@ class EditProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Edit Product')));
+    return const Scaffold(
+      appBar: CustomAppBar(title: 'Edit Product'),
+      body: Center(child: Text('Edit Product')),
+    );
   }
 }
