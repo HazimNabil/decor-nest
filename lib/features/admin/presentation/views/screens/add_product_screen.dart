4112285e0c1f9a894b455517d3431909b9ebf30a
HazimNabil <HazemNabil95@outlook.com>
2025-08-22 03:04:41 +0300

    feat: add app bar to AddProductScreen

--- lib/features/admin/presentation/views/screens/add_product_screen.dart
+++ lib/features/admin/presentation/views/screens/add_product_screen.dart
@@ -1,3 +1,4 @@
+import 'package:decor_nest/core/widgets/custom_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class AddProductScreen extends StatelessWidget {
@@ -7,6 +8,9 @@ class AddProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Center(child: Text('Add Product')));
+    return const Scaffold(
+      appBar: CustomAppBar(title: 'Add Product'),
+      body: Center(child: Text('Add Product')),
+    );
   }
 }
