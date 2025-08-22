dd45cc3a5dd42f7f6806c58564477cc0de334c5e
HazimNabil <HazemNabil95@outlook.com>
2025-08-22 03:54:39 +0300

    feat: add hint to CustomDropDownButton

--- lib/features/admin/presentation/views/widgets/custom_drop_down_button.dart
+++ lib/features/admin/presentation/views/widgets/custom_drop_down_button.dart
@@ -3,13 +3,15 @@ import 'package:decor_nest/core/themes/app_styles.dart';
 import 'package:flutter/material.dart';
 
 class CustomDropDownButton extends StatefulWidget {
-  final String currentValue;
+  final String? currentValue;
+  final String? hint;
   final List<String> values;
 
   const CustomDropDownButton({
     super.key,
-    required this.currentValue,
+    this.currentValue,
     required this.values,
+    this.hint,
   });
 
   @override
@@ -17,7 +19,7 @@ class CustomDropDownButton extends StatefulWidget {
 }
 
 class _CustomDropDownButtonState extends State<CustomDropDownButton> {
-  late String _selectedValue;
+  String? _selectedValue;
 
   @override
   void initState() {
@@ -34,11 +36,12 @@ class _CustomDropDownButtonState extends State<CustomDropDownButton> {
       ),
       child: DropdownButton<String>(
         value: _selectedValue,
+        hint: widget.hint != null ? Text(widget.hint!) : null,
         elevation: 0,
         isExpanded: true,
         borderRadius: BorderRadius.circular(12),
         underline: const SizedBox.shrink(),
-        padding: const EdgeInsets.symmetric(horizontal: 16),
+        padding: const EdgeInsets.symmetric(horizontal: 12),
         menuMaxHeight: 200,
         style: AppStyles.medium16(context),
         items: List.generate(
@@ -48,7 +51,7 @@ class _CustomDropDownButtonState extends State<CustomDropDownButton> {
             child: Text(widget.values[index]),
           ),
         ),
-        onChanged: (value) => setState(() => _selectedValue = value!),
+        onChanged: (value) => setState(() => _selectedValue = value),
       ),
     );
   }
