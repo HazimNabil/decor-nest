04688f8a019c6ccdb1da1b03e16b16286d34a26f
HazimNabil <HazemNabil95@outlook.com>
2025-08-22 20:37:15 +0300

    feat: create Product model

---
+++ lib/core/models/product.dart
@@ -0,0 +1,37 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'product.g.dart';
+
+@JsonSerializable(includeIfNull: false)
+class Product {
+  final int? id;
+  final String name;
+  final String description;
+  final double price;
+  final int stock;
+  final String category;
+  @JsonKey(name: 'wood_type')
+  final String? woodType;
+  @JsonKey(name: 'image_url')
+  final String? imageUrl;
+  @JsonKey(name: 'image_path')
+  final String? imagePath;
+
+  const Product({
+    this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.stock,
+    required this.category,
+    this.woodType,
+    this.imageUrl,
+    this.imagePath,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return _$ProductFromJson(json);
+  }
+
+  Map<String, dynamic> toJson() => _$ProductToJson(this);
+}
