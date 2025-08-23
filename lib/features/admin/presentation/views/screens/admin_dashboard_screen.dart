2ae795a584a192f34be35584a55312c5767b21d2
HazimNabil <HazemNabil95@outlook.com>
2025-08-23 20:37:20 +0300

    feat: provide ReadProductsBloc to AdminDashboardScreenBody

--- lib/features/admin/presentation/views/screens/admin_dashboard_screen.dart
+++ lib/features/admin/presentation/views/screens/admin_dashboard_screen.dart
@@ -1,5 +1,9 @@
+import 'package:decor_nest/core/di/service_locator.dart';
+import 'package:decor_nest/features/admin/data/repos/admin_repo_impl.dart';
+import 'package:decor_nest/features/admin/presentation/view_models/read_products_bloc/read_products_bloc.dart';
 import 'package:decor_nest/features/admin/presentation/views/widgets/admin_dashboard_screen_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AdminDashboardScreen extends StatelessWidget {
   static const path = '/admin_dashboard';
@@ -8,6 +12,16 @@ class AdminDashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: SafeArea(child: AdminDashboardScreenBody()));
+    return Scaffold(
+      body: SafeArea(
+        child: BlocProvider(
+          create: (context) {
+            return ReadProductsBloc(locator<AdminRepoImpl>())
+              ..add(const ProductsFetched());
+          },
+          child: const AdminDashboardScreenBody(),
+        ),
+      ),
+    );
   }
 }
