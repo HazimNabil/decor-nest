edb4ccbd7a3f53c859e312cfd137aa6477c8ae1b
HazimNabil <HazemNabil95@outlook.com>
2025-08-23 19:54:40 +0300

    feat: implement product pagination in ReadProductsBloc

--- lib/features/admin/presentation/view_models/read_products_bloc/read_products_bloc.dart
+++ lib/features/admin/presentation/view_models/read_products_bloc/read_products_bloc.dart
@@ -1,4 +1,6 @@
+import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:decor_nest/core/models/product.dart';
+import 'package:decor_nest/features/admin/data/repos/admin_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
@@ -6,9 +8,41 @@ part 'read_products_event.dart';
 part 'read_products_state.dart';
 
 class ReadProductsBloc extends Bloc<ReadProductsEvent, ReadProductsState> {
-  ReadProductsBloc() : super(const ReadProductsState()) {
-    on<ProductsFetched>((event, emit) {
-      // TODO: implement event handler
-    });
+  final AdminRepo _adminRepo;
+
+  ReadProductsBloc(this._adminRepo) : super(const ReadProductsState()) {
+    on<ProductsFetched>(_readProducts, transformer: droppable());
+  }
+
+  Future<void> _readProducts(
+    ProductsFetched event,
+    Emitter<ReadProductsState> emit,
+  ) async {
+    if (state.hasReachedMax) return;
+
+    if (state.page == 0) {
+      emit(state.copyWith(status: ReadProductsStatus.loading));
+    }
+
+    final result = await _adminRepo.readProducts(page: state.page);
+    result.fold(
+      (failure) => emit(
+        state.copyWith(
+          status: ReadProductsStatus.failure,
+          errorMessage: failure.message,
+        ),
+      ),
+      (products) {
+        final oldProducts = state.products;
+        emit(
+          state.copyWith(
+            status: ReadProductsStatus.success,
+            products: oldProducts + products,
+            hasReachedMax: products.length < 10,
+            page: state.page + 1,
+          ),
+        );
+      },
+    );
   }
 }
