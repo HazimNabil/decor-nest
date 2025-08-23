4f379529bcb3a45eeaf428d566418c085fafbc66
HazimNabil <HazemNabil95@outlook.com>
2025-08-23 19:56:53 +0300

    feat: register admin-related services in service locator

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -1,13 +1,24 @@
+import 'package:decor_nest/core/services/database_service.dart';
+import 'package:decor_nest/features/admin/data/repos/admin_repo_impl.dart';
+import 'package:decor_nest/features/admin/data/services/storage_service.dart';
 import 'package:decor_nest/features/auth/data/repos/auth_repo_impl.dart';
 import 'package:decor_nest/features/auth/data/services/auth_service.dart';
 import 'package:get_it/get_it.dart';
 
-final GetIt locator = GetIt.instance;
+final locator = GetIt.instance;
 
 void setupServiceLocator() {
   locator.registerLazySingleton<AuthService>(() => AuthService());
 
   locator.registerLazySingleton<AuthRepoImpl>(
     () => AuthRepoImpl(locator<AuthService>()),
+  );
+
+  locator.registerLazySingleton<DatabaseService>(() => DatabaseService());
+
+  locator.registerLazySingleton<StorageService>(() => StorageService());
+
+  locator.registerLazySingleton<AdminRepoImpl>(
+    () => AdminRepoImpl(locator<DatabaseService>(), locator<StorageService>()),
   );
 }
