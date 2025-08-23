0f33d5e8349ab1d0bf8ae8ff6416233b13768b3b
HazimNabil <HazemNabil95@outlook.com>
2025-08-23 20:34:43 +0300

    refactor: extract AdminDashboardScreenBody widget

--- lib/features/admin/presentation/views/screens/admin_dashboard_screen.dart
+++ lib/features/admin/presentation/views/screens/admin_dashboard_screen.dart
@@ -1,11 +1,5 @@
-import 'package:decor_nest/core/helper/extensions.dart';
-import 'package:decor_nest/core/themes/app_styles.dart';
-import 'package:decor_nest/core/widgets/custom_button.dart';
-import 'package:decor_nest/features/admin/presentation/views/screens/add_product_screen.dart';
-import 'package:decor_nest/features/admin/presentation/views/widgets/admin_product_sliver_list.dart';
-import 'package:decor_nest/features/admin/presentation/views/widgets/search_field.dart';
+import 'package:decor_nest/features/admin/presentation/views/widgets/admin_dashboard_screen_body.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class AdminDashboardScreen extends StatelessWidget {
   static const path = '/admin_dashboard';
@@ -14,40 +8,6 @@ class AdminDashboardScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24),
-          child: CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Text('Products', style: AppStyles.semiBold32(context)),
-                    const SizedBox(height: 8),
-                    Text(
-                      'Manage your products',
-                      style: AppStyles.regular16(context),
-                    ),
-                    const SizedBox(height: 24),
-                    const SearchField(),
-                    const SizedBox(height: 16),
-                    CustomButton(
-                      text: 'Add Product',
-                      color: context.primaryColor,
-                      onPressed: () => context.push(AddProductScreen.path),
-                    ),
-                    const SizedBox(height: 16),
-                  ],
-                ),
-              ),
-              const AdminProductSliverList(),
-            ],
-          ),
-        ),
-      ),
-    );
+    return const Scaffold(body: SafeArea(child: AdminDashboardScreenBody()));
   }
 }
---
+++ lib/features/admin/presentation/views/widgets/admin_dashboard_screen_body.dart
@@ -0,0 +1,47 @@
+import 'package:decor_nest/core/helper/extensions.dart';
+import 'package:decor_nest/core/themes/app_styles.dart';
+import 'package:decor_nest/core/widgets/custom_button.dart';
+import 'package:decor_nest/features/admin/presentation/views/screens/add_product_screen.dart';
+import 'package:decor_nest/features/admin/presentation/views/widgets/admin_product_sliver_list.dart';
+import 'package:decor_nest/features/admin/presentation/views/widgets/search_field.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class AdminDashboardScreenBody extends StatelessWidget {
+  const AdminDashboardScreenBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(24),
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Products', style: AppStyles.semiBold32(context)),
+                const SizedBox(height: 8),
+                Text(
+                  'Manage your products',
+                  style: AppStyles.regular16(context),
+                ),
+                const SizedBox(height: 24),
+                const SearchField(),
+                const SizedBox(height: 16),
+                CustomButton(
+                  text: 'Add Product',
+                  color: context.primaryColor,
+                  onPressed: () => context.push(AddProductScreen.path),
+                ),
+                const SizedBox(height: 16),
+              ],
+            ),
+          ),
+          const AdminProductSliverList(),
+        ],
+      ),
+    );
+  }
+}
