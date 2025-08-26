8f36020c19aad411c15da719780efdd66d283776
HazimNabil <HazemNabil95@outlook.com>
2025-08-26 20:50:07 +0300

    feat: determine AddProductCubit states

---
+++ lib/features/admin/presentation/view_models/add_product_cubit/add_product_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'add_product_state.dart';
+
+class AddProductCubit extends Cubit<AddProductState> {
+  AddProductCubit() : super(const AddProductInitial());
+}
