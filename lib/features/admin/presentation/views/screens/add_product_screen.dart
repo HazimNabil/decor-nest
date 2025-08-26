a370b942700efe197ec360b4886e1f7ddc9ed658
HazimNabil <HazemNabil95@outlook.com>
2025-08-26 21:32:40 +0300

    feat: provide AddProductCubit to AddProductScreenBody

--- lib/features/admin/presentation/views/screens/add_product_screen.dart
+++ lib/features/admin/presentation/views/screens/add_product_screen.dart
@@ -1,6 +1,10 @@
+import 'package:decor_nest/core/di/service_locator.dart';
 import 'package:decor_nest/core/widgets/custom_app_bar.dart';
+import 'package:decor_nest/features/admin/data/repos/admin_repo_impl.dart';
 import 'package:decor_nest/features/admin/presentation/views/widgets/add_product_screen_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:decor_nest/features/admin/presentation/view_models/add_product_cubit/add_product_cubit.dart';
 
 class AddProductScreen extends StatelessWidget {
   static const path = '/add_product';
@@ -9,9 +13,12 @@ class AddProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(title: 'Add Product'),
-      body: AddProductScreenBody(),
+    return Scaffold(
+      appBar: const CustomAppBar(title: 'Add Product'),
+      body: BlocProvider(
+        create: (context) => AddProductCubit(locator<AdminRepoImpl>()),
+        child: const AddProductScreenBody(),
+      ),
     );
   }
 }
