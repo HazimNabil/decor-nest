52d44e5781bdc9423d9dcdd74a6429048490e984
HazimNabil <HazemNabil95@outlook.com>
2025-08-29 02:03:05 +0300

    feat: add editProduct method to EditProductCubit

--- lib/features/admin/presentation/view_models/edit_product_cubit/edit_product_cubit.dart
+++ lib/features/admin/presentation/view_models/edit_product_cubit/edit_product_cubit.dart
@@ -1,8 +1,25 @@
+import 'package:decor_nest/core/models/product.dart';
+import 'package:decor_nest/features/admin/data/repos/admin_repo.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart' show Cubit;
 
 part 'edit_product_state.dart';
 
 class EditProductCubit extends Cubit<EditProductState> {
-  EditProductCubit() : super(const EditProductInitial());
+  final AdminRepo _adminRepo;
+
+  EditProductCubit(this._adminRepo) : super(const EditProductInitial());
+
+  Future<void> editProduct(Product product, Map<String, dynamic> fields) async {
+    emit(const EditProductLoading());
+
+    final result = await _adminRepo.updateProduct(
+      product: product,
+      fields: fields,
+    );
+    result.fold(
+      (failure) => emit(EditProductFailure(failure.message)),
+      (_) => emit(const EditProductSuccess()),
+    );
+  }
 }
