02456c0e71f908624a241bc7fd267f1d3340d1b6
HazimNabil <HazemNabil95@outlook.com>
2025-08-29 02:04:51 +0300

    feat: provide EditProductCubit to EditProductScreenBody

--- lib/features/admin/presentation/views/screens/edit_product_screen.dart
+++ lib/features/admin/presentation/views/screens/edit_product_screen.dart
@@ -1,7 +1,11 @@
+import 'package:decor_nest/core/di/service_locator.dart';
 import 'package:decor_nest/core/models/product.dart';
 import 'package:decor_nest/core/widgets/custom_app_bar.dart';
+import 'package:decor_nest/features/admin/data/repos/admin_repo_impl.dart';
+import 'package:decor_nest/features/admin/presentation/view_models/edit_product_cubit/edit_product_cubit.dart';
 import 'package:decor_nest/features/admin/presentation/views/widgets/edit_product_screen_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class EditProductScreen extends StatelessWidget {
   static const path = '/edit_product';
@@ -13,7 +17,10 @@ class EditProductScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(title: 'Edit Product'),
-      body: EditProductScreenBody(product: product),
+      body: BlocProvider(
+        create: (_) => EditProductCubit(locator<AdminRepoImpl>()),
+        child: EditProductScreenBody(product: product),
+      ),
     );
   }
 }
