dc75f3bd96f04523f39c6f5042c3040642b3785a
HazimNabil <HazemNabil95@outlook.com>
2025-09-01 04:17:27 +0300

    feat: integrate and trigger EditProductCubit

--- lib/features/admin/presentation/views/widgets/edit_product_screen_body.dart
+++ lib/features/admin/presentation/views/widgets/edit_product_screen_body.dart
@@ -4,10 +4,14 @@ import 'package:decor_nest/core/themes/app_styles.dart';
 import 'package:decor_nest/core/widgets/custom_button.dart';
 import 'package:decor_nest/core/widgets/labeled_field.dart';
 import 'package:decor_nest/features/admin/data/models/product_input_data.dart';
+import 'package:decor_nest/features/admin/presentation/view_models/edit_product_cubit/edit_product_cubit.dart';
 import 'package:decor_nest/features/admin/presentation/views/widgets/custom_drop_down_button.dart';
 import 'package:decor_nest/features/admin/presentation/views/widgets/edit_product_form.dart';
 import 'package:decor_nest/features/admin/presentation/views/widgets/update_image_placeholder.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
+import 'package:toastification/toastification.dart';
 
 class EditProductScreenBody extends StatelessWidget {
   final Product product;
@@ -68,14 +72,42 @@ class EditProductScreenBody extends StatelessWidget {
               productInputData: _productInputData,
             ),
             const SizedBox(height: 24),
-            CustomButton(
-              text: 'Update Product',
-              color: context.primaryColor,
-              onPressed: () => _formKey.currentState!.save(),
+            BlocConsumer<EditProductCubit, EditProductState>(
+              listener: (context, state) {
+                if (state is EditProductSuccess) {
+                  context.showToast(
+                    message: 'Product edited successfully',
+                    type: ToastificationType.success,
+                  );
+                  context.pop();
+                } else if (state is EditProductFailure) {
+                  context.showToast(
+                    message: state.message,
+                    type: ToastificationType.error,
+                  );
+                }
+              },
+              builder: (context, state) {
+                return CustomButton(
+                  text: 'Update Product',
+                  color: context.primaryColor,
+                  isLoading: state is EditProductLoading,
+                  onPressed: () async => await editProduct(context),
+                );
+              },
             ),
           ],
         ),
       ),
+    );
+  }
+
+  Future<void> editProduct(BuildContext context) async {
+    _formKey.currentState!.save();
+    await context.read<EditProductCubit>().editProduct(
+      fields: _productInputData,
+      product: product,
+      image: _productInputData.image,
     );
   }
 
