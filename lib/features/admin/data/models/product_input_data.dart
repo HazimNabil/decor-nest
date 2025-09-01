9c82d343d08364520761fc84146befca29e0516d
HazimNabil <HazemNabil95@outlook.com>
2025-09-01 04:05:10 +0300

    feat: add toJson method to ProductInputData model

--- lib/features/admin/data/models/product_input_data.dart
+++ lib/features/admin/data/models/product_input_data.dart
@@ -1,11 +1,24 @@
 import 'dart:io' show File;
 
+import 'package:json_annotation/json_annotation.dart';
+
+part 'product_input_data.g.dart';
+
+@JsonSerializable(includeIfNull: false, createFactory: false)
 class ProductInputData {
   String? name;
   String? description;
   double? price;
   int? stock;
   String? category;
+  @JsonKey(name: 'wood_type')
   String? woodType;
+  @JsonKey(includeToJson: false)
   File? image;
+
+  Map<String, dynamic> toJson() {
+    final json = _$ProductInputDataToJson(this);
+    json.removeWhere((key, value) => value is String ? value.isEmpty : false);
+    return json;
+  }
 }
