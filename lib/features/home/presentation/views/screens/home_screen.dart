5628eba7a05edfd6099f41849188f16a1b3c515f
HazimNabil <HazemNabil95@outlook.com>
2025-09-02 23:34:41 +0300

    feat: add HomeScreenAppBar to HomeScreen

--- lib/features/home/presentation/views/screens/home_screen.dart
+++ lib/features/home/presentation/views/screens/home_screen.dart
@@ -1,14 +1,18 @@
+import 'package:decor_nest/features/home/presentation/views/widgets/home_screen_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   static const path = '/home';
+
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Home'),
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 24),
+      child: Scaffold(
+        appBar: HomeScreenAppBar(),
+        body: Center(child: Text('Home')),
       ),
     );
   }
---
+++ lib/features/home/presentation/views/widgets/home_screen_app_bar.dart
@@ -0,0 +1,45 @@
+import 'package:decor_nest/core/helper/assets.dart';
+import 'package:decor_nest/core/helper/extensions.dart';
+import 'package:decor_nest/core/themes/app_styles.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class HomeScreenAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const HomeScreenAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      surfaceTintColor: Colors.transparent,
+      elevation: 0,
+      scrolledUnderElevation: 0,
+      title: RichText(
+        text: TextSpan(
+          text: 'Welcome,\n',
+          style: AppStyles.regular13(context),
+          children: [
+            TextSpan(text: 'John Doe', style: AppStyles.medium16(context)),
+          ],
+        ),
+      ),
+      actions: [
+        CircleAvatar(
+          radius: 24,
+          backgroundColor: context.surfaceColor,
+          child: IconButton(
+            icon: SvgPicture.asset(
+              Assets.iconsSearch,
+              height: 19,
+              colorFilter: ColorFilter.mode(context.textColor, BlendMode.srcIn),
+            ),
+            onPressed: () {},
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
