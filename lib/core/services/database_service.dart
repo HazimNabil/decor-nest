b9555cf8632939c9a902483074c438e024e427fe
HazimNabil <HazemNabil95@outlook.com>
2025-09-04 01:40:20 +0300

    feat: add filterByCategory method to DatabaseService

--- lib/core/services/database_service.dart
+++ lib/core/services/database_service.dart
@@ -1,3 +1,4 @@
+import 'package:decor_nest/core/helper/typedefs.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:decor_nest/core/models/product.dart';
 
@@ -24,10 +25,7 @@ class DatabaseService {
     await _supabase.from(tableName).delete().eq('id', id);
   }
 
-  Future<List<Map<String, dynamic>>> read({
-    required String tableName,
-    required int page,
-  }) async {
+  FutureJson read({required String tableName, required int page}) async {
     final start = page * _pageSize;
     final end = start + _pageSize - 1;
 
@@ -40,7 +38,25 @@ class DatabaseService {
     return data;
   }
 
-  Future<List<Map<String, dynamic>>> search({
+  FutureJson filterByCategory({
+    required String tableName,
+    required int page,
+    required String category,
+  }) async {
+    final start = page * _pageSize;
+    final end = start + _pageSize - 1;
+
+    final data = await _supabase
+        .from(tableName)
+        .select()
+        .eq('category', category)
+        .order('created_at', ascending: false)
+        .range(start, end);
+
+    return data;
+  }
+
+  FutureJson search({
     required String tableName,
     required String query,
     required int page,
