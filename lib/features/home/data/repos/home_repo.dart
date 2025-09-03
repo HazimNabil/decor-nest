7ae84a06f37509bc73ce2bf4258a14442e6b7aba
HazimNabil <HazemNabil95@outlook.com>
2025-09-04 01:46:18 +0300

    feat: create HomeRepo interface

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,6 @@
+import 'package:decor_nest/core/models/product.dart';
+import 'package:decor_nest/core/helper/typedefs.dart';
+
+abstract interface class HomeRepo {
+  FutureEither<List<Product>> fetchProducts({required int page, String? category});
+}
