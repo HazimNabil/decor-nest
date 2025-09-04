2f19f727bc7cdc6ac33117ca96a7f9211b61969a
HazimNabil <HazemNabil95@outlook.com>
2025-09-04 03:35:08 +0300

    feat: provide FetchProductsBloc to HomeScreenBody

--- lib/features/home/presentation/views/screens/home_screen.dart
+++ lib/features/home/presentation/views/screens/home_screen.dart
@@ -1,6 +1,10 @@
+import 'package:decor_nest/core/di/service_locator.dart';
+import 'package:decor_nest/features/home/data/repos/home_repo_impl.dart';
+import 'package:decor_nest/features/home/presentation/view_models/fetch_products_bloc/fetch_products_bloc.dart';
 import 'package:decor_nest/features/home/presentation/views/widgets/home_screen_app_bar.dart';
 import 'package:decor_nest/features/home/presentation/views/widgets/home_screen_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   static const path = '/home';
@@ -9,9 +13,15 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: HomeScreenAppBar(),
-      body: HomeScreenBody(),
+    return Scaffold(
+      appBar: const HomeScreenAppBar(),
+      body: BlocProvider(
+        create: (context) {
+          return FetchProductsBloc(locator<HomeRepoImpl>())
+            ..add(const ProductsFetched());
+        },
+        child: const HomeScreenBody(),
+      ),
     );
   }
 }
