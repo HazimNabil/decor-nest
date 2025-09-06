eeb87c3aca91c63ed72df43ceea6fa6e75c585d5
HazimNabil <HazemNabil95@outlook.com>
2025-09-07 01:16:21 +0300

    feat: add toggleFavorite to ToggleFavoriteCubit

--- lib/features/home/presentation/view_models/toggle_favorite_cubit/toggle_favorite_cubit.dart
+++ lib/features/home/presentation/view_models/toggle_favorite_cubit/toggle_favorite_cubit.dart
@@ -1,8 +1,29 @@
+import 'package:decor_nest/features/favorites/data/models/favorite_product.dart';
+import 'package:decor_nest/features/favorites/data/repos/favorites_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part 'toggle_favorite_state.dart';
 
 class ToggleFavoriteCubit extends Cubit<ToggleFavoriteState> {
-  ToggleFavoriteCubit() : super(const ToggleFavoriteInitial());
+  final FavoritesRepo favoritesRepo;
+
+  ToggleFavoriteCubit(this.favoritesRepo)
+    : super(const ToggleFavoriteInitial());
+
+  Future<void> toggleFavorite({
+    required FavoriteProduct favorite,
+    required bool isFavorite,
+  }) async {
+    emit(const ToggleFavoriteLoading());
+
+    final result = await favoritesRepo.toggleFavorite(
+      favorite: favorite,
+      isFavorite: isFavorite,
+    );
+    result.fold(
+      (failure) => emit(ToggleFavoriteFailure(failure.message)),
+      (_) => emit(const ToggleFavoriteSuccess()),
+    );
+  }
 }
