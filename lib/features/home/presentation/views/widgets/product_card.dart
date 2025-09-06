eca9e951ba5e5eef427ec3016b1e1cda4074fa40
HazimNabil <HazemNabil95@outlook.com>
2025-09-07 01:48:21 +0300

    feat: integrate ToggleFavoriteCubit

--- lib/features/home/presentation/views/widgets/product_card.dart
+++ lib/features/home/presentation/views/widgets/product_card.dart
@@ -12,6 +12,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:toastification/toastification.dart';
 
 class ProductCard extends StatelessWidget {
   final Product product;
@@ -20,50 +21,60 @@ class ProductCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        GestureDetector(
-          onTap: () => context.push(DetailsScreen.path, extra: product),
-          child: Stack(
-            children: [
-              ClipRRect(
-                borderRadius: BorderRadius.circular(14),
-                child: CachedNetworkImage(
-                  imageUrl: product.imageUrl!,
-                  errorWidget: (_, _, _) => const Icon(Icons.error),
-                ),
-              ),
-              Positioned(
-                top: 10,
-                right: 10,
-                child: CircleAvatar(
-                  radius: 15,
-                  backgroundColor: context.surfaceColor,
-                  child: IconButton(
-                    icon: SvgPicture.asset(Assets.iconsUnselectedFavorites),
-                    onPressed: () async => await toggleFavorite(context),
+    return BlocListener<ToggleFavoriteCubit, ToggleFavoriteState>(
+      listener: (context, state) {
+        if (state is ToggleFavoriteFailure) {
+          context.showToast(
+            message: state.message,
+            type: ToastificationType.error,
+          );
+        }
+      },
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          GestureDetector(
+            onTap: () => context.push(DetailsScreen.path, extra: product),
+            child: Stack(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(14),
+                  child: CachedNetworkImage(
+                    imageUrl: product.imageUrl!,
+                    errorWidget: (_, _, _) => const Icon(Icons.error),
                   ),
                 ),
-              ),
-            ],
+                Positioned(
+                  top: 10,
+                  right: 10,
+                  child: CircleAvatar(
+                    radius: 15,
+                    backgroundColor: context.surfaceColor,
+                    child: IconButton(
+                      icon: SvgPicture.asset(Assets.iconsUnselectedFavorites),
+                      onPressed: () async => await toggleFavorite(context),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
-        ),
-        const SizedBox(height: 10),
-        Text(
-          product.name,
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-          style: AppStyles.medium16(context),
-        ),
-        const SizedBox(height: 4),
-        Text(
-          '\$${product.price.toStringAsFixed(2)}',
-          style: AppStyles.medium16(
-            context,
-          ).copyWith(color: context.actionColor),
-        ),
-      ],
+          const SizedBox(height: 10),
+          Text(
+            product.name,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: AppStyles.medium16(context),
+          ),
+          const SizedBox(height: 4),
+          Text(
+            '\$${product.price.toStringAsFixed(2)}',
+            style: AppStyles.medium16(
+              context,
+            ).copyWith(color: context.actionColor),
+          ),
+        ],
+      ),
     );
   }
 
