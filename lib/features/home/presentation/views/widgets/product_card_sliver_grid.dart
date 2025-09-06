1a73afb02f799c8aea9d7555638779b6ab89c977
HazimNabil <HazemNabil95@outlook.com>
2025-09-07 01:33:53 +0300

    feat: provide ToggleFavoriteCubit to ProductCard

--- lib/features/home/presentation/views/widgets/product_card_sliver_grid.dart
+++ lib/features/home/presentation/views/widgets/product_card_sliver_grid.dart
@@ -1,6 +1,10 @@
+import 'package:decor_nest/core/di/service_locator.dart';
 import 'package:decor_nest/core/models/product.dart';
+import 'package:decor_nest/features/favorites/data/repos/favorites_repo_impl.dart';
 import 'package:decor_nest/features/home/presentation/views/widgets/product_card.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:decor_nest/features/home/presentation/view_models/toggle_favorite_cubit/toggle_favorite_cubit.dart';
 
 class ProductCardSliverGrid extends StatelessWidget {
   final List<Product> products;
@@ -9,17 +13,20 @@ class ProductCardSliverGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SliverGrid.builder(
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        mainAxisSpacing: 16,
-        crossAxisSpacing: 16,
-        childAspectRatio: 0.71,
+    return BlocProvider(
+      create: (context) => ToggleFavoriteCubit(locator<FavoritesRepoImpl>()),
+      child: SliverGrid.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          mainAxisSpacing: 16,
+          crossAxisSpacing: 16,
+          childAspectRatio: 0.71,
+        ),
+        itemCount: products.length,
+        itemBuilder: (_, index) {
+          return ProductCard(product: products[index]);
+        },
       ),
-      itemCount: products.length,
-      itemBuilder: (_, index) {
-        return ProductCard(product: products[index]);
-      },
     );
   }
 }
