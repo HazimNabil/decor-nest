87a2fc0ca17b6e53f2ce7821774f5a73a94b43f9
HazimNabil <HazemNabil95@outlook.com>
2025-09-11 00:32:37 +0300

    feat: implement interactive quantity selector in ProductActionBar

--- lib/features/home/presentation/views/widgets/product_action_bar.dart
+++ lib/features/home/presentation/views/widgets/product_action_bar.dart
@@ -4,11 +4,34 @@ import 'package:decor_nest/core/models/product.dart';
 import 'package:decor_nest/core/themes/app_styles.dart';
 import 'package:decor_nest/core/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
+import 'package:toastification/toastification.dart';
 
-class ProductActionBar extends StatelessWidget {
+class ProductActionBar extends StatefulWidget {
   final Product product;
 
   const ProductActionBar({super.key, required this.product});
+
+  @override
+  State<ProductActionBar> createState() => _ProductActionBarState();
+}
+
+class _ProductActionBarState extends State<ProductActionBar> {
+  late final ValueNotifier<int> _quantityNotifier;
+  late final ValueNotifier<double> _totalPriceNotifier;
+
+  @override
+  void initState() {
+    super.initState();
+    _quantityNotifier = ValueNotifier(1);
+    _totalPriceNotifier = ValueNotifier(widget.product.price);
+  }
+
+  @override
+  void dispose() {
+    _quantityNotifier.dispose();
+    _totalPriceNotifier.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,24 +54,57 @@ class ProductActionBar extends StatelessWidget {
                 radius: 13,
                 backgroundColor: context.backgroundColor,
                 foregroundColor: context.subTextColor,
-                child: const Icon(Icons.remove),
+                child: IconButton(
+                  padding: EdgeInsets.zero,
+                  icon: const Icon(Icons.remove),
+                  onPressed: () {
+                    if (_quantityNotifier.value > 1) {
+                      _quantityNotifier.value--;
+                      _totalPriceNotifier.value -= widget.product.price;
+                    }
+                  },
+                ),
               ),
-              Text(
-                '01',
-                style: AppStyles.regular14(
-                  context,
-                ).copyWith(color: context.textColor),
+              ValueListenableBuilder(
+                valueListenable: _quantityNotifier,
+                builder: (context, value, _) {
+                  return Text(
+                    value.toString(),
+                    style: AppStyles.regular14(
+                      context,
+                    ).copyWith(color: context.textColor),
+                  );
+                },
               ),
               CircleAvatar(
                 radius: 13,
                 backgroundColor: context.primaryColor,
                 foregroundColor: Colors.white,
-                child: const Icon(Icons.add),
+                child: IconButton(
+                  padding: EdgeInsets.zero,
+                  icon: const Icon(Icons.add),
+                  onPressed: () {
+                    if (_quantityNotifier.value < widget.product.stock) {
+                      _quantityNotifier.value++;
+                      _totalPriceNotifier.value += widget.product.price;
+                    } else {
+                      context.showToast(
+                        message: 'Stock is not available',
+                        type: ToastificationType.error,
+                      );
+                    }
+                  },
+                ),
               ),
               const Spacer(),
-              Text(
-                'Total: \$${product.price.toStringAsFixed(2)}',
-                style: AppStyles.medium16(context),
+              ValueListenableBuilder(
+                valueListenable: _totalPriceNotifier,
+                builder: (context, value, _) {
+                  return Text(
+                    'Total: \$${value.toStringAsFixed(2)}',
+                    style: AppStyles.medium16(context),
+                  );
+                },
               ),
             ],
           ),
