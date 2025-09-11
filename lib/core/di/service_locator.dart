5635d5c0cb14a2993238de9b4f39a2144467ac4a
HazimNabil <HazemNabil95@outlook.com>
2025-09-11 03:38:21 +0300

    feat: register CartRepoImpl to service locator

--- lib/core/di/service_locator.dart
+++ lib/core/di/service_locator.dart
@@ -3,6 +3,7 @@ import 'package:decor_nest/features/admin/data/repos/admin_repo_impl.dart';
 import 'package:decor_nest/features/admin/data/services/storage_service.dart';
 import 'package:decor_nest/features/auth/data/repos/auth_repo_impl.dart';
 import 'package:decor_nest/features/auth/data/services/auth_service.dart';
+import 'package:decor_nest/features/cart/data/repos/cart_repo_impl.dart';
 import 'package:decor_nest/features/favorites/data/repos/favorites_repo_impl.dart';
 import 'package:decor_nest/features/home/data/repos/home_repo_impl.dart';
 import 'package:get_it/get_it.dart';
@@ -30,5 +31,9 @@ void setupServiceLocator() {
 
   locator.registerLazySingleton<FavoritesRepoImpl>(
     () => FavoritesRepoImpl(locator<DatabaseService>()),
+  );
+
+  locator.registerLazySingleton<CartRepoImpl>(
+    () => CartRepoImpl(locator<DatabaseService>()),
   );
 }
