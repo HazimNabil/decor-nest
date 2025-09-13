01923b5f259324e9b7e9cdec9767411c98c5d56c
HazimNabil <HazemNabil95@outlook.com>
2025-09-13 06:01:01 +0300

    feat: provide FavoritesCubit to FavoritesScreenBody

--- lib/features/favorites/presentation/views/screens/favorites_screen.dart
+++ lib/features/favorites/presentation/views/screens/favorites_screen.dart
@@ -1,15 +1,22 @@
+import 'package:decor_nest/core/di/service_locator.dart';
+import 'package:decor_nest/features/favorites/data/repos/favorites_repo_impl.dart';
 import 'package:decor_nest/features/favorites/presentation/views/widgets/favorites_screen_app_bar.dart';
 import 'package:decor_nest/features/favorites/presentation/views/widgets/favorites_screen_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:decor_nest/features/favorites/presentation/favorites_cubit/favorites_cubit.dart';
 
 class FavoritesScreen extends StatelessWidget {
   const FavoritesScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: FavoritesScreenAppBar(),
-      body: FavoritesScreenBody(),
+    return Scaffold(
+      appBar: const FavoritesScreenAppBar(),
+      body: BlocProvider(
+        create: (context) => FavoritesCubit(locator<FavoritesRepoImpl>()),
+        child: const FavoritesScreenBody(),
+      ),
     );
   }
 }
