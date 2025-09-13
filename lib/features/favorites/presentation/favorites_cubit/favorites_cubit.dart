7ee5f8af99486b8bc2ba42259c2f74d57182ef4f
HazimNabil <HazemNabil95@outlook.com>
2025-09-13 05:57:08 +0300

    feat: listen to favorites stream in FavoritesCubit

--- lib/features/favorites/presentation/favorites_cubit/favorites_cubit.dart
+++ lib/features/favorites/presentation/favorites_cubit/favorites_cubit.dart
@@ -1,9 +1,34 @@
+import 'dart:async';
+
 import 'package:decor_nest/features/favorites/data/models/favorite_product.dart';
+import 'package:decor_nest/features/favorites/data/repos/favorites_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part 'favorites_state.dart';
 
 class FavoritesCubit extends Cubit<FavoritesState> {
-  FavoritesCubit() : super(FavoritesInitial());
+  final FavoritesRepo _favoritesRepo;
+  late final StreamSubscription _favoritesSubscription;
+
+  FavoritesCubit(this._favoritesRepo) : super(FavoritesInitial()) {
+    _listenToFavorites();
+  }
+
+  Future<void> _listenToFavorites() async {
+    emit(FavoritesLoadInProgress());
+    final favoritesStream = await _favoritesRepo.watchFavorites();
+    _favoritesSubscription = favoritesStream.listen((result) {
+      result.fold(
+        (failure) => emit(FavoritesLoadFailure(failure.message)),
+        (favorites) => emit(FavoritesLoaded(favorites)),
+      );
+    });
+  }
+
+  @override
+  Future<void> close() {
+    _favoritesSubscription.cancel();
+    return super.close();
+  }
 }
