d1594e75f79e4edd19de322b523411909ea41833
HazimNabil <HazemNabil95@outlook.com>
2025-09-20 05:23:07 +0300

    feat: add operations to CartRepo

--- lib/features/cart/data/repos/cart_repo.dart
+++ lib/features/cart/data/repos/cart_repo.dart
@@ -1,7 +1,11 @@
 import 'package:decor_nest/core/helper/typedefs.dart';
+import 'package:decor_nest/core/models/product.dart';
 import 'package:decor_nest/features/cart/data/models/cart_product.dart';
 import 'package:fpdart/fpdart.dart' show Unit;
 
 abstract interface class CartRepo {
-  FutureEither<Unit> addToCart(CartProduct cartProduct);
+  FutureEither<Unit> addToCart(Product product);
+  FutureEither<Unit> removeFromCart(CartProduct cartProduct);
+  Future<bool> isInCart(Product product);
+  StreamEither<List<CartProduct>> watchCart();
 }
