6ea25a57f1df43933a49111466f66b915c693949
HazimNabil <HazemNabil95@outlook.com>
2025-09-22 00:43:19 +0300

    feat: add ProductCardSliverGrid to SearchScreen

--- lib/features/search/presentation/views/screens/search_screen.dart
+++ lib/features/search/presentation/views/screens/search_screen.dart
@@ -1,3 +1,5 @@
+import 'package:decor_nest/core/models/product.dart';
+import 'package:decor_nest/features/home/presentation/views/widgets/product_card_sliver_grid.dart';
 import 'package:decor_nest/features/search/presentation/views/widgets/search_header.dart';
 import 'package:flutter/material.dart';
 
@@ -6,12 +8,16 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.all(16),
+          padding: const EdgeInsets.only(top: 16, left: 16, right: 16),
           child: CustomScrollView(
-            slivers: [SliverToBoxAdapter(child: SearchHeader())],
+            slivers: [
+              const SliverToBoxAdapter(child: SearchHeader()),
+              const SliverToBoxAdapter(child: SizedBox(height: 24)),
+              ProductCardSliverGrid(products: List.filled(12, Product.dummy())),
+            ],
           ),
         ),
       ),
