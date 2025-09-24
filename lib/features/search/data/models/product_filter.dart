fdc9f4feec6674ac9e0063bcd8af9bdf48cc4eea
HazimNabil <HazemNabil95@outlook.com>
2025-09-25 00:36:12 +0300

    refactor: make ProductFilter model mutable

--- lib/features/search/data/models/product_filter.dart
+++ lib/features/search/data/models/product_filter.dart
@@ -1,15 +1,15 @@
 class ProductFilter {
-  final List<String>? category;
-  final List<String>? woodType;
-  final double? minPrice;
-  final double? maxPrice;
-  final String? searchQuery;
-  final String? sortBy;
-  final bool ascending;
+  List<String>? categories;
+  List<String>? woodTypes;
+  double? minPrice;
+  double? maxPrice;
+  String? searchQuery;
+  String? sortBy;
+  bool ascending;
 
-  const ProductFilter({
-    this.category,
-    this.woodType,
+  ProductFilter({
+    this.categories,
+    this.woodTypes,
     this.minPrice,
     this.maxPrice,
     this.searchQuery,
@@ -17,27 +17,7 @@ class ProductFilter {
     this.ascending = false,
   });
 
-  ProductFilter copyWith({
-    List<String>? category,
-    List<String>? woodType,
-    double? minPrice,
-    double? maxPrice,
-    String? searchQuery,
-    String? sortBy,
-    bool? ascending,
-  }) {
-    return ProductFilter(
-      category: category ?? this.category,
-      woodType: woodType ?? this.woodType,
-      minPrice: minPrice ?? this.minPrice,
-      maxPrice: maxPrice ?? this.maxPrice,
-      searchQuery: searchQuery ?? this.searchQuery,
-      sortBy: sortBy ?? this.sortBy,
-      ascending: ascending ?? this.ascending,
-    );
-  }
-
   ProductFilter clear() {
-    return const ProductFilter();
+    return ProductFilter();
   }
 }
