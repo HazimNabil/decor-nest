c7b4e246bd2aa73915543ccb39380f1a49f09d52
HazimNabil <HazemNabil95@outlook.com>
2025-09-30 01:46:38 +0300

    feat: trigger and integrate ProfileDataCubit

--- lib/features/profile/presentation/views/screens/profile_screen.dart
+++ lib/features/profile/presentation/views/screens/profile_screen.dart
@@ -1,13 +1,16 @@
+import 'package:decor_nest/core/constants/auth_constants.dart';
 import 'package:decor_nest/core/di/service_locator.dart';
 import 'package:decor_nest/core/helper/extensions.dart';
 import 'package:decor_nest/core/themes/app_styles.dart';
 import 'package:decor_nest/features/profile/data/repos/profile_repo_impl.dart';
 import 'package:decor_nest/features/profile/presentation/view_models/logout_cubit/logout_cubit.dart';
+import 'package:decor_nest/features/profile/presentation/view_models/profile_data_cubit/profile_data_cubit.dart';
 import 'package:decor_nest/features/profile/presentation/views/widgets/logout_button.dart';
 import 'package:decor_nest/features/profile/presentation/views/widgets/profile_option_tile_list.dart';
 import 'package:decor_nest/features/profile/presentation/views/widgets/theme_segmented_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({super.key});
@@ -21,9 +24,23 @@ class ProfileScreen extends StatelessWidget {
           child: Center(
             child: Column(
               children: [
-                Text('John Doe', style: AppStyles.medium20(context)),
+                BlocBuilder<ProfileDataCubit, User?>(
+                  builder: (context, user) {
+                    return Text(
+                      user!.userMetadata![UserConstants.username],
+                      style: AppStyles.medium20(context),
+                    );
+                  },
+                ),
                 const SizedBox(height: 2),
-                Text('john.doe@gmail.com', style: AppStyles.regular14(context)),
+                BlocBuilder<ProfileDataCubit, User?>(
+                  builder: (context, user) {
+                    return Text(
+                      user!.email!,
+                      style: AppStyles.regular14(context),
+                    );
+                  },
+                ),
                 const SizedBox(height: 32),
                 const ThemeSegmentedButton(),
                 Divider(
