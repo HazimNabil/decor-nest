b8ea9537cc9622ba5e2043aa674d43a5b1182654
HazimNabil <HazemNabil95@outlook.com>
2025-09-30 00:49:24 +0300

    feat: determine LogoutCubit states

---
+++ lib/features/profile/presentation/view_models/logout_cubit/logout_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'logout_state.dart';
+
+class LogoutCubit extends Cubit<LogoutState> {
+  LogoutCubit() : super(const LogoutInitial());
+}
