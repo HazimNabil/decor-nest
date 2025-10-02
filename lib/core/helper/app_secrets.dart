d0678cd160d83cabb63e4b272553f7747ed35e62
HazimNabil <HazemNabil95@outlook.com>
2025-10-02 09:54:31 +0300

    chore: add Paymob secrets to AppSecrets

--- lib/core/helper/app_secrets.dart
+++ lib/core/helper/app_secrets.dart
@@ -6,8 +6,19 @@ part 'app_secrets.g.dart';
 abstract class AppSecrets {
   @EnviedField(varName: 'SUPABASE_URL', obfuscate: true)
   static final String supabaseUrl = _AppSecrets.supabaseUrl;
+
   @EnviedField(varName: 'SUPABASE_ANON_KEY', obfuscate: true)
   static final String supabaseAnonKey = _AppSecrets.supabaseAnonKey;
+
   @EnviedField(varName: 'WEB_CLIENT_ID', obfuscate: true)
   static final String webClientId = _AppSecrets.webClientId;
+
+  @EnviedField(varName: 'PAYMOB_API_KEY', obfuscate: true)
+  static final String paymobApiKey = _AppSecrets.paymobApiKey;
+
+  @EnviedField(varName: 'PAYMOB_IFRAME_ID', obfuscate: true)
+  static final int paymobIFrameId = _AppSecrets.paymobIFrameId;
+
+  @EnviedField(varName: 'PAYMOB_INTEGRATION_ID', obfuscate: true)
+  static final int paymobIntegrationId = _AppSecrets.paymobIntegrationId;
 }
