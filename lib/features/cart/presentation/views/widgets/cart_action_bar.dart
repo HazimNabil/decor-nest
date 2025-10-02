88738b0d69754e3d6e215998a0608c6c5285aa9c
HazimNabil <HazemNabil95@outlook.com>
2025-10-02 22:13:30 +0300

    feat: integrate and trigger PaymentCubit

--- lib/features/cart/presentation/views/widgets/cart_action_bar.dart
+++ lib/features/cart/presentation/views/widgets/cart_action_bar.dart
@@ -1,7 +1,11 @@
 import 'package:decor_nest/core/helper/extensions.dart';
 import 'package:decor_nest/core/themes/app_styles.dart';
 import 'package:decor_nest/core/widgets/custom_button.dart';
+import 'package:decor_nest/features/cart/data/models/payment_request.dart';
+import 'package:decor_nest/features/cart/presentation/view_models/payment_cubit/payment_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 
 class CartActionBar extends StatelessWidget {
   final double totalPayment;
@@ -33,10 +37,105 @@ class CartActionBar extends StatelessWidget {
             ],
           ),
           const SizedBox(height: 24),
-          CustomButton(
-            text: 'Check out',
-            color: context.primaryColor,
-            onPressed: () {},
+          BlocConsumer<PaymentCubit, PaymentState>(
+            listener: (context, state) {
+              if (state is PaymentSuccess) {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text(
+                      'Payment Successful',
+                      style: AppStyles.medium20(
+                        context,
+                      ).copyWith(color: context.primaryColor),
+                      textAlign: TextAlign.center,
+                    ),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const SizedBox(height: 8),
+                        Icon(
+                          Icons.check_circle,
+                          color: context.primaryColor,
+                          size: 56,
+                        ),
+                        const SizedBox(height: 16),
+                        Text(
+                          'Payment has been successfully processed.',
+                          style: AppStyles.regular16(
+                            context,
+                          ).copyWith(color: context.textColor),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 24),
+                        CustomButton(
+                          text: 'OK',
+                          color: context.primaryColor,
+                          onPressed: () => context.pop(),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              } else if (state is PaymentFailure) {
+                showDialog(
+                  context: context,
+                  builder: (context) => AlertDialog(
+                    title: Text(
+                      'Payment Failed',
+                      style: AppStyles.medium20(
+                        context,
+                      ).copyWith(color: Colors.red),
+                      textAlign: TextAlign.center,
+                    ),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const SizedBox(height: 8),
+                        const Icon(Icons.error, color: Colors.red, size: 56),
+                        const SizedBox(height: 16),
+                        Text(
+                          state.message,
+                          style: AppStyles.regular16(
+                            context,
+                          ).copyWith(color: context.textColor),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 24),
+                        CustomButton(
+                          text: 'OK',
+                          color: Colors.red,
+                          onPressed: () => context.pop(),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              }
+            },
+            builder: (context, state) {
+              return CustomButton(
+                text: 'Check out',
+                color: context.primaryColor,
+                isLoading: state is PaymentLoading,
+                onPressed: () async {
+                  final paymentRequest = PaymentRequest(
+                    amount: totalPayment,
+                    context: context,
+                    title: Text(
+                      'Checkout',
+                      style: AppStyles.medium24(
+                        context,
+                      ).copyWith(color: Colors.white),
+                    ),
+                    appBarColor: context.primaryColor,
+                  );
+                  await context.read<PaymentCubit>().processPayment(
+                    paymentRequest,
+                  );
+                },
+              );
+            },
           ),
         ],
       ),
