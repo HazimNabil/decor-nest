ff6729c91476196532a9cb33905e49d5fe5a1ee4
HazimNabil <HazemNabil95@outlook.com>
2025-10-04 04:47:34 +0300

    feat: provide and trigger FetchOrdersCubit

--- lib/features/orders/presentation/views/screens/order_history_screen.dart
+++ lib/features/orders/presentation/views/screens/order_history_screen.dart
@@ -1,6 +1,10 @@
+import 'package:decor_nest/core/di/service_locator.dart';
 import 'package:decor_nest/core/widgets/custom_app_bar.dart';
+import 'package:decor_nest/features/orders/data/repos/orders_repo_impl.dart';
+import 'package:decor_nest/features/orders/presentation/view_models/fetch_orders_cubit/fetch_orders_cubit.dart';
 import 'package:decor_nest/features/orders/presentation/views/widgets/order_history_screen_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class OrderHistoryScreen extends StatelessWidget {
   static const path = '/order_history';
@@ -9,9 +13,14 @@ class OrderHistoryScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: CustomAppBar(title: 'Order History'),
-      body: OrderHistoryScreenBody(),
+    return Scaffold(
+      appBar: const CustomAppBar(title: 'Order History'),
+      body: BlocProvider(
+        create: (_) {
+          return FetchOrdersCubit(locator<OrdersRepoImpl>())..fetchOrders();
+        },
+        child: const OrderHistoryScreenBody(),
+      ),
     );
   }
 }
