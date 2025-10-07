2db35a9c5159e089614cf57b81a706e03b2d356d
HazimNabil <HazemNabil95@outlook.com>
2025-10-07 21:04:04 +0300

    feat: provide and integrate InternetConnectionCubit

--- lib/decor_nest.dart
+++ lib/decor_nest.dart
@@ -1,29 +1,50 @@
+import 'package:decor_nest/features/offline/presentation/views/screens/offline_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:decor_nest/core/themes/app_themes.dart';
 import 'package:decor_nest/core/router/app_router.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:decor_nest/core/themes/theme_cubit.dart';
+import 'package:decor_nest/core/view_models/internet_connection_cubit.dart';
 
-class DecorNest extends StatelessWidget {
+class DecorNest extends StatefulWidget {
   const DecorNest({super.key});
 
   @override
+  State<DecorNest> createState() => _DecorNestState();
+}
+
+class _DecorNestState extends State<DecorNest> {
+  bool _wasOffline = false;
+
+  @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ThemeCubit, ThemeMode>(
-      builder: (context, themeMode) {
-        return MaterialApp.router(
-          debugShowCheckedModeBanner: false,
-          locale: DevicePreview.locale(context),
-          builder: DevicePreview.appBuilder,
-          theme: AppThemes.light,
-          darkTheme: AppThemes.dark,
-          themeMode: themeMode,
-          themeAnimationDuration: const Duration(seconds: 1),
-          themeAnimationCurve: Curves.fastOutSlowIn,
-          routerConfig: AppRouter.router,
-        );
+    return BlocListener<InternetConnectionCubit, bool?>(
+      listener: (context, isConnected) async {
+        if (isConnected == false) {
+          _wasOffline = true;
+          AppRouter.router.push(OfflineScreen.path);
+        } else if (isConnected == true && _wasOffline) {
+          _wasOffline = false;
+          AppRouter.router.pop();
+        }
+        await Future.delayed(const Duration(seconds: 5));
       },
+      child: BlocBuilder<ThemeCubit, ThemeMode>(
+        builder: (context, themeMode) {
+          return MaterialApp.router(
+            debugShowCheckedModeBanner: false,
+            locale: DevicePreview.locale(context),
+            builder: DevicePreview.appBuilder,
+            theme: AppThemes.light,
+            darkTheme: AppThemes.dark,
+            themeMode: themeMode,
+            themeAnimationDuration: const Duration(seconds: 1),
+            themeAnimationCurve: Curves.fastOutSlowIn,
+            routerConfig: AppRouter.router,
+          );
+        },
+      ),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:decor_nest/core/helper/app_secrets.dart';
 import 'package:decor_nest/core/helper/cache_helper.dart';
 import 'package:decor_nest/core/helper/custom_bloc_observer.dart';
 import 'package:decor_nest/core/themes/theme_cubit.dart';
+import 'package:decor_nest/core/view_models/internet_connection_cubit.dart';
 import 'package:decor_nest/decor_nest.dart';
 import 'package:decor_nest/features/cart/data/services/payment_service.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart' show kReleaseMode;
 import 'package:flutter/material.dart' show runApp, WidgetsFlutterBinding;
-import 'package:flutter_bloc/flutter_bloc.dart' show Bloc, BlocProvider;
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
@@ -28,8 +30,15 @@ Future<void> main() async {
       // ignore: avoid_redundant_argument_values
       enabled: !kReleaseMode,
       builder: (_) {
-        return BlocProvider(
-          create: (_) => ThemeCubit(),
+        return MultiBlocProvider(
+          providers: [
+            BlocProvider(create: (_) => ThemeCubit()),
+            BlocProvider(
+              create: (_) {
+                return InternetConnectionCubit(locator<InternetConnection>());
+              },
+            ),
+          ],
           child: const DecorNest(),
         );
       },
