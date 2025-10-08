09efb0e3ffa95db71cc327efa3db6122a217e5fc
HazimNabil <HazemNabil95@outlook.com>
2025-10-08 23:04:55 +0300

    refactor: extract initApp as separated method

---
+++ lib/core/helper/app_initializer.dart
@@ -0,0 +1,22 @@
+import 'package:decor_nest/core/di/service_locator.dart';
+import 'package:decor_nest/core/helper/custom_bloc_observer.dart';
+import 'package:decor_nest/core/helper/cache_helper.dart';
+import 'package:decor_nest/features/cart/data/services/payment_service.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:decor_nest/core/helper/app_secrets.dart';
+
+Future<void> initApp() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Future.wait([
+    CacheHelper.init(),
+    Supabase.initialize(
+      url: AppSecrets.supabaseUrl,
+      anonKey: AppSecrets.supabaseAnonKey,
+    ),
+  ]);
+  setupServiceLocator();
+  await locator.isReady<PaymentService>();
+  Bloc.observer = CustomBlocObserver();
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,15 @@
-import 'package:decor_nest/core/di/service_locator.dart';
-import 'package:decor_nest/core/helper/app_secrets.dart';
-import 'package:decor_nest/core/helper/cache_helper.dart';
-import 'package:decor_nest/core/helper/custom_bloc_observer.dart';
+import 'package:decor_nest/core/helper/app_initializer.dart';
 import 'package:decor_nest/core/view_models/theme_cubit.dart';
 import 'package:decor_nest/core/view_models/internet_connection_cubit.dart';
 import 'package:decor_nest/decor_nest.dart';
-import 'package:decor_nest/features/cart/data/services/payment_service.dart';
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/foundation.dart' show kReleaseMode;
-import 'package:flutter/material.dart' show runApp, WidgetsFlutterBinding;
+import 'package:flutter/material.dart' show runApp;
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Future.wait([
-    CacheHelper.init(),
-    Supabase.initialize(
-      url: AppSecrets.supabaseUrl,
-      anonKey: AppSecrets.supabaseAnonKey,
-    ),
-  ]);
-  setupServiceLocator();
-  await locator.isReady<PaymentService>();
-  Bloc.observer = CustomBlocObserver();
+  await initApp();
   runApp(
     DevicePreview(
       // ignore: avoid_redundant_argument_values
