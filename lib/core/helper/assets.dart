fafd0184a4afb61cffd492c5a59e99974a40c6b3
HazimNabil <HazemNabil95@outlook.com>
2025-10-20 00:29:05 +0300

    chore: add screenshots to app assets

--- lib/core/helper/assets.dart
+++ lib/core/helper/assets.dart
@@ -7,8 +7,6 @@ class Assets {
 
   static const iconsFilter = 'assets/icons/filter.svg';
 
-  static const iconsTrash = 'assets/icons/trash.svg';
-
   static const iconsGoogle = 'assets/icons/google.svg';
 
   static const iconsSelectedFavorites = 'assets/icons/selected_favorites.svg';
@@ -18,6 +16,8 @@ class Assets {
   static const iconsSelectedProfile = 'assets/icons/selected_profile.svg';
 
   static const iconsSelectedSearch = 'assets/icons/selected_search.svg';
+
+  static const iconsTrash = 'assets/icons/trash.svg';
 
   static const iconsUnselectedFavorites =
       'assets/icons/unselected_favorites.svg';
@@ -30,17 +30,15 @@ class Assets {
 
   static const imagesAndroid12Splash = 'assets/images/android_12_splash.png';
 
+  static const imagesEmptyCart = 'assets/images/empty_cart.svg';
+
   static const imagesFailure = 'assets/images/failure.svg';
+
+  static const imagesLamp = 'assets/images/lamp.jpeg';
 
   static const imagesNoData = 'assets/images/no_data.svg';
 
-  static const imagesEmptyCart = 'assets/images/empty_cart.svg';
-
   static const imagesNoResultsFound = 'assets/images/no_results_found.svg';
-
-  static const imagesTypeToSearch = 'assets/images/type_to_search.svg';
-
-  static const imagesLamp = 'assets/images/lamp.jpeg';
 
   static const imagesOnBoardingOne = 'assets/images/on_boarding_one.png';
 
@@ -49,4 +47,6 @@ class Assets {
   static const imagesOnBoardingTwo = 'assets/images/on_boarding_two.png';
 
   static const imagesSplash = 'assets/images/splash.png';
+
+  static const imagesTypeToSearch = 'assets/images/type_to_search.svg';
 }
